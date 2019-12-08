9b0f25c11cee2898c15d3a8befb73353fb37366b
Ark1700 <Ark1700@yandex.ru>
2019-12-09 00:21:15 +0300

    Представил компоненты в виде классов

--- src/components/task.js
+++ src/components/task.js
@@ -1,6 +1,5 @@
-import {MonthNames} from '../const.js';
-import {formatTime} from '../utils.js';
-
+import {MONTH_NAMES} from '../const.js';
+import {createElement, formatTime} from '../utils.js';
 
 const createHashtagsMarkup = (hashtags) => {
   return hashtags
@@ -17,13 +16,16 @@ const createHashtagsMarkup = (hashtags) => {
 };
 
 
-export const createTaskTemplate = (task) => {
+const createTaskTemplate = (task) => {
+  // Подсказка:
+  // Все работу производим заранее. Внутри шаблонной строки никаких вычислений не делаем,
+  // потому что внутри большой разметки сложно искать какой-либо код.
   const {description, tags, dueDate, color, repeatingDays} = task;
 
   const isExpired = dueDate instanceof Date && dueDate < Date.now();
   const isDateShowing = !!dueDate;
 
-  const date = isDateShowing ? `${dueDate.getDate()} ${MonthNames[dueDate.getMonth()]}` : ``;
+  const date = isDateShowing ? `${dueDate.getDate()} ${MONTH_NAMES[dueDate.getMonth()]}` : ``;
   const time = isDateShowing ? formatTime(dueDate) : ``;
 
   const hashtags = createHashtagsMarkup(Array.from(tags));
@@ -82,3 +84,27 @@ export const createTaskTemplate = (task) => {
     </article>`
   );
 };
+
+export default class Task {
+  constructor(task) {
+    this._task = task;
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this._task);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
